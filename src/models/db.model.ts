f01b5ade34cd9b901a7bb78346be3b2fc8f8c17f
briangor <briangor13@gmail.com>
2024-10-30 21:50:59 +0300

    update db.model.ts

--- src/models/db.model.ts
+++ src/models/db.model.ts
@@ -1,13 +1,13 @@
-import mysql from "mysql2";
-// import { dbConfig } from '../config/db.config.js';
+import * as mysql from 'mysql2';
+import { dbConfig } from '../config/db.config.js';
 
 //todo: check use of pool
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: 'dbConfig.HOST',
-  user: 'dbConfig.USER',
-  password: 'dbConfig.PASSWORD',
-  database: 'dbConfig.DATABASE',
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DATABASE,
 });
 
 // open the MySQL connection
