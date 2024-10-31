3f313abccfc2bf9f51265bac96249ac86b1b1824
briangor <briangor13@gmail.com>
2024-10-31 17:57:05 +0300

    update helper.ts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,1 +1,6 @@
 // Contains utility functions (e.g., validation, formatting)
+
+export const isValidDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return !isNaN(date.getTime());
+}
