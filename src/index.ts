89ff68da1bae8ebfce9598dae62e3a726a6ba11a
briangor <briangor13@gmail.com>
2025-10-16 23:31:14 +0300

    update server_port variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import mysql2 from 'mysql2';
 import movieRoutes from './routes/movie.routes';
 
 dotenv.config();
@@ -9,7 +8,7 @@ dotenv.config();
 const app: Express = express();
 // const URL = (process.env.TYPE = 'production') ? 'https://movie-recommendation.vercel.app' : 'http://localhost' 
 const url = 'http://localhost' 
-const port = process.env.PORT || 3100;
+const port = process.env.SERVER_PORT || 3100;
 
 const corsOptions: any = {
     origin: [ 'http://127.0.0.1:3000', process.env.PROD_URL],
